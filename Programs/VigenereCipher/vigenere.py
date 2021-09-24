2e70d6a38382f7b527f0678061466078ce91c0ab
Timothy O <timoliver200@gmail.com>
2021-09-24 15:00:49 -0500

    disabled the mandatory read and checked for a flag before process func.

--- Programs/VigenereCipher/vigenere.py
+++ Programs/VigenereCipher/vigenere.py
@@ -19,83 +19,78 @@ def checkCase(letter):
 # encoding message function
 def process(message):
 
-    if (sys.argv[1] != ("-e" or "-d")):
-        outputString = "Should I encode (python -e) or decode (python -d)?"
+    text = []
+    keyList = []
+    currIndex = 0
+    outputString = ""
 
-    else:
+    # put each individual character in a list (for comparison w/ characters in key)
+    for char in message:
+        text.append(char)
+    
+    # get key and compare length to that of text; repeat key if key is smaller
+    key = sys.argv[2]
+    for char in key:
+        if char != " ":
+            keyList.append(char)
 
-        text = []
-        keyList = []
-        currIndex = 0
-        outputString = ""
+    while (len(keyList) < len(text)):
+        keyList.append(keyList[currIndex])
+        currIndex += 1
+    currIndex = 0
 
-        # put each individual character in a list (for comparison w/ characters in key)
-        for char in message:
-            text.append(char)
-        
-        # get key and compare length to that of text; repeat key if key is smaller
-        key = sys.argv[2]
-        for char in key:
-            if char != " ":
-                keyList.append(char)
-
-        while (len(keyList) < len(text)):
-            keyList.append(keyList[currIndex])
-            currIndex += 1
-        currIndex = 0
-
-        if sys.argv[1] == "-e":
-            for i in range(0,len(text)):
-                # plaintext character is a capital letter
-                if checkCase(text[i]) == "upper":
-                    if checkCase(keyList[currIndex]) == "lower":
-                        encodedChar = chr(((((ord(text[i])-65) + (ord(keyList[currIndex])-32-65))) % 26) + 65)  # need getting the correct character when the cases of the text and key are different - T.O.
-                    else:
-                        encodedChar = chr(((((ord(text[i])-65) + (ord(keyList[currIndex])-65))) % 26) + 65)
-                    outputString += encodedChar
-                    currIndex += 1
-                # plaintext character is a lowercase letter
-                elif checkCase(text[i]) == "lower":
-                    if checkCase(keyList[currIndex]) == "upper":
-                        encodedChar = chr(((((ord(text[i])-97) + (ord(keyList[currIndex])-65))) % 26) + 97)  # need getting the correct character when the cases of the text and key are different - T.O.
-                    else:
-                        encodedChar = chr(((((ord(text[i])-97) + (ord(keyList[currIndex])-97))) % 26) + 97)
-                    outputString += encodedChar
-                    currIndex += 1
-                # if the plaintext character is a newline character
-                elif (text[i] == "\n"):
-                    outputString += "\n"
-                    currIndex = 0
-                # plaintext character is anything else, just repeat the character      
-                else:
-                    outputString += text[i]
-
-                    
-        elif sys.argv[1] == "-d":
-            for i in range(0,len(text)):
+    if sys.argv[1] == "-e":
+        for i in range(0,len(text)):
             # plaintext character is a capital letter
-                if checkCase(text[i]) == "upper":
-                    if checkCase(keyList[currIndex]) == "lower":
-                        encodedChar = chr(((((ord(text[i])-65) - (ord(keyList[currIndex])-32-65))) % 26) + 65)  # need getting the correct character when the cases of the text and key are different - T.O.
-                    else:
-                        encodedChar = chr(((((ord(text[i])-65) - (ord(keyList[currIndex])-65))) % 26) + 65)
-                    outputString += encodedChar
-                    currIndex += 1
-                # plaintext character is a lowercase letter
-                elif checkCase(text[i]) == "lower":
-                    if checkCase(keyList[currIndex]) == "upper":
-                        encodedChar = chr(((((ord(text[i])-97) - (ord(keyList[currIndex])+32-97))) % 26) + 97)  # need getting the correct character when the cases of the text and key are different - T.O.
-                    else:
-                        encodedChar = chr(((((ord(text[i])-97) - (ord(keyList[currIndex])-97))) % 26) + 97)
-                    outputString += encodedChar
-                    currIndex += 1
-                # if the plaintext character is a newline character
-                elif (text[i] == "\n"):
-                    outputString += "\n"
-                    currIndex = 0  
-                # plaintext character is anything else, just repeat the character      
+            if checkCase(text[i]) == "upper":
+                if checkCase(keyList[currIndex]) == "lower":
+                    encodedChar = chr(((((ord(text[i])-65) + (ord(keyList[currIndex])-32-65))) % 26) + 65)  # need getting the correct character when the cases of the text and key are different - T.O.
                 else:
-                    outputString += text[i]
+                    encodedChar = chr(((((ord(text[i])-65) + (ord(keyList[currIndex])-65))) % 26) + 65)
+                outputString += encodedChar
+                currIndex += 1
+            # plaintext character is a lowercase letter
+            elif checkCase(text[i]) == "lower":
+                if checkCase(keyList[currIndex]) == "upper":
+                    encodedChar = chr(((((ord(text[i])-97) + (ord(keyList[currIndex])-65))) % 26) + 97)  # need getting the correct character when the cases of the text and key are different - T.O.
+                else:
+                    encodedChar = chr(((((ord(text[i])-97) + (ord(keyList[currIndex])-97))) % 26) + 97)
+                outputString += encodedChar
+                currIndex += 1
+            # if the plaintext character is a newline character
+            elif (text[i] == "\n"):
+                outputString += "\n"
+                currIndex = 0
+            # plaintext character is anything else, just repeat the character      
+            else:
+                outputString += text[i]
+
+                
+    elif sys.argv[1] == "-d":
+        for i in range(0,len(text)):
+        # plaintext character is a capital letter
+            if checkCase(text[i]) == "upper":
+                if checkCase(keyList[currIndex]) == "lower":
+                    encodedChar = chr(((((ord(text[i])-65) - (ord(keyList[currIndex])-32-65))) % 26) + 65)  # need getting the correct character when the cases of the text and key are different - T.O.
+                else:
+                    encodedChar = chr(((((ord(text[i])-65) - (ord(keyList[currIndex])-65))) % 26) + 65)
+                outputString += encodedChar
+                currIndex += 1
+            # plaintext character is a lowercase letter
+            elif checkCase(text[i]) == "lower":
+                if checkCase(keyList[currIndex]) == "upper":
+                    encodedChar = chr(((((ord(text[i])-97) - (ord(keyList[currIndex])+32-97))) % 26) + 97)  # need getting the correct character when the cases of the text and key are different - T.O.
+                else:
+                    encodedChar = chr(((((ord(text[i])-97) - (ord(keyList[currIndex])-97))) % 26) + 97)
+                outputString += encodedChar
+                currIndex += 1
+            # if the plaintext character is a newline character
+            elif (text[i] == "\n"):
+                outputString += "\n"
+                currIndex = 0  
+            # plaintext character is anything else, just repeat the character      
+            else:
+                outputString += text[i]
 
 
     # output the appropriate message
@@ -109,7 +104,12 @@ def process(message):
 
 # differentiate between arguments of encoding or decoding
 if __name__ == "__main__":
-    process(sys.stdin.read())
+    message = sys.stdin.read()
+    if (sys.argv[1] in ["-e", "-d"]):
+        process(message)
+    else:
+        sys.stdout.write("Should I encode (python -e) or decode (python -d)?")
+        
 
 
 ## Git Changes
