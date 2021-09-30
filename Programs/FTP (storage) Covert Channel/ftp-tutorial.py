a4eebcaf21a9497121d9a8d23f9584bf0ffe263d
JChancey00 <91154386+JChancey00@users.noreply.github.com>
2021-09-30 14:49:33 -0500

    Added header, comments below, and attempted 10 bit method

--- Programs/FTP (storage) Covert Channel/ftp-tutorial.py
+++ Programs/FTP (storage) Covert Channel/ftp-tutorial.py
@@ -1,9 +1,15 @@
+##################################
+# Team Name:    The Fool
+# Assignment:   Binary Decoder
+# Date:         29 September 2021
+##################################
+
 # use Python 3
   
 from ftplib import FTP
 import sys
 # specifies the number of bits the cipher will be broken down into
-METHOD = "7"
+METHOD = "10"
 
 # FTP server details
 IP = "138.47.157.5"
@@ -27,6 +33,8 @@ ftp.dir(files.append)
 # exit the FTP server
 ftp.quit()
 outputString = ""
+permissions = ""
+binary = ""
 # display the folder contents
 for f in files:
 	if METHOD == "7":
@@ -38,8 +46,30 @@ for f in files:
 					binary += "0"
 				else:
 					binary += "1"
-#			print(permissions)
-#			print(binary)
 			outputString += chr(int(binary, 2))
+	elif METHOD == "10":
+		permissions += f[:10]
+		for i in range(len(permissions)):
+			if permissions[i] == "-":
+				binary += "0"
+			else:
+				binary += "1"
+			if len(binary) == 7:
+				newBinary = "0b{}".format(binary)
+				outputString += chr(int(newBinary, 2))
+				binary = ""
 
 print(outputString)
+
+
+## Git Changes
+    
+# Format:
+##  Name of coder; Date of change
+##  Description of changes
+
+##  Josh, John, Jason; 9-29-21
+##  Added 7 bit method and got it to work
+
+##  Jason; 9-30-21
+##  Attempted to add 10 bit method, message will print out but with a lot of "noise" before and after
