5d37de8e72f953b02b23627f4f4c0b4754082939
Zach Hoover <38825744+ZachHoover@users.noreply.github.com>
2021-10-21 20:11:45 -0500

    fixed some technical stuff

--- Challenges/Keystroke Dynamics/typing-mod.py
+++ Challenges/Keystroke Dynamics/typing-mod.py
@@ -1,24 +1,48 @@
-from pynput.keyboard import Controller
-from time import sleep
-from random import uniform
+from pynput.keyboard import Listener, Key
 from termios import tcflush, TCIFLUSH
-from sys import stdout
-from typing2 import getInfo
+from time import time
+from sys import stdin, stdout
 
-(string, keypress, keyinterval) = getInfo()
+string = []
+keypress = []
+keyinterval = []
+keys = ""
+intervals = ""
+times = ""
+global t_0, t_1, t0, t1
 
-keyboard = Controller()
+def pressing(key):
+    if (key == Key.esc):
+        return False
+    global t_0, t_1, t0, t1
+    if (len(keypress) > 0):
+        t_1 = time()
+        keyinterval.append(t_1-t_0)
+    try:
+       t0 = time()
+       string.append(key.char)
+       #print (key.char)#print(key.char.encode("ascii"), end = ' ')
+    except AttributeError:
+       t0 = time()
+       string.append(str(key))
 
-index = 0
-for index in range(0,len(string)):
-    if (index >0):
-        sleep(keyinterval[index-1])
-    keyboard.press(string[index])
-    sleep(keypress[index])
-    keyboard.release(string[index])
-    
 
-tcflush(stdout, TCIFLUSH)
-print()
+def releasing(key):
+    global t_0, t_1, t0, t1
+    t1 = t_0 = time()
+    keypress.append(t1-t0)
 
 
+with Listener(on_press=pressing, on_release=releasing) as listener:
+    listener.join()
+
+tcflush(stdin, TCIFLUSH)
+
+for i in range(1,len(string)):
+    string.append(f"{string[i-1]}{string[i]}")
+
+for i in range(0,len(keyinterval)):
+    keypress.append(keyinterval[i])
+stdout.write(", ".join(string))
+stdout.write("\n")
+stdout.write(str(keypress).strip("[]"))
