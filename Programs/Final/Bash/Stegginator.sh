481255da03299beb884297b7673d93c7fb6b8d6c
Zach Hoover <38825744+ZachHoover@users.noreply.github.com>
2021-11-08 11:43:14 -0600

    Add files via upload

--- Programs/Final/Bash/Stegginator.sh
+++ Programs/Final/Bash/Stegginator.sh
@@ -1,1 +1,16 @@
+output=out.jpg
+input=stegged-byte.bmp
+for i in {256..1024..128}
+do
+	for j in {1..8..1}
+	do
+		python3 Steg.py -o$i -i$j -B -r -w"$input" > O"$i"I"$j""$output"
+		size=`wc -c < O"$i"I"$j""$output"`
+		if [ $size == 0 ]
+		then
+			rm O"$i"I"$j""$output"
+		fi
+	done
+done
+
 
