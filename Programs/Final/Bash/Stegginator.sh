8e86b5eea0c524a46b19503f38d267246a4317af
Zach Hoover <38825744+ZachHoover@users.noreply.github.com>
2021-11-08 11:46:40 -0600

    Update Stegginator.sh

--- Programs/Final/Bash/Stegginator.sh
+++ Programs/Final/Bash/Stegginator.sh
@@ -1,9 +1,13 @@
+#Change input file to stegged file and output extension to what file you believe you'll get (no extension if you are unsure)
 output=out.jpg
 input=stegged-byte.bmp
+
+#You may have to modify the intervals {start..end..increment}
 for i in {256..1024..128}
 do
 	for j in {1..8..1}
 	do
+		#Make sure bit or byte mod is properly selected.
 		python3 Steg.py -o$i -i$j -B -r -w"$input" > O"$i"I"$j""$output"
 		size=`wc -c < O"$i"I"$j""$output"`
 		if [ $size == 0 ]
